23374b2a816ffad880a2859ea1f5d7f1e9abe8ad
Dementedduck <155681526+Dementedduck@users.noreply.github.com>
2024-09-17 17:45:47 +0100

    Add algorithm Add loop

--- sorting_algorithm.rb
+++ sorting_algorithm.rb
@@ -1,3 +1,23 @@
-def bubble_sort(array)
+test_array = [4, 3, 78, 2, 0, 2, 222, 19, 2, 31]
 
+def bubble_sort(array)
+  loop_amount = array.length
+  swapped = false
+  loop do
+    (loop_amount - 1).times do |count|
+
+      if array[count] > array[count + 1]
+
+        array[count], array[count + 1] = array[count + 1], array[count]
+
+        swapped = true
+    end
+  end
+
+  break if not swapped
+  swapped = false
+  end
+  puts array
 end
+
+bubble_sort(test_array)
